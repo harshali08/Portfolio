27916989b91bf988a631730cc4932cfc71a39422
harshali <harshalifarde@gmail.com>
2023-07-01 01:39:05 +0530

    projcard donee

--- src/components/ProjCard.jsx
+++ src/components/ProjCard.jsx
@@ -14,7 +14,7 @@ const ProjCard = (props) => {
                 <div class="service-icon_2 d-flex justify-content-center align-items-center"><a href={props.link}> <BsBoxArrowInUpRight fontSize={25}  color="black"></BsBoxArrowInUpRight></a></div>
                
                 <div class="service-content_2">
-                <img src={props.s} height={175} style={{width:"100%", borderRadius:"10px"}}></img>
+                <img src={props.s} height={170} style={{width:"100%", borderRadius:"10px"}}></img>
                     <h4 class="title_2 mx-2" style={{textAlign:"center"}}>{props.title}</h4>
                     {/* <p class="description_2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque.</p> */}
                     <a href="#" class="read-more"><PiGithubLogoFill ></PiGithubLogoFill></a>
