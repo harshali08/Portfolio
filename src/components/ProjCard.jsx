68ec7f5a2d2e366652fdc5210c23c6c2cfe425ca
harshali <harshalifarde@gmail.com>
2023-07-01 01:44:21 +0530

    github links added

--- src/components/ProjCard.jsx
+++ src/components/ProjCard.jsx
@@ -17,7 +17,7 @@ const ProjCard = (props) => {
                 <img src={props.s} height={170} style={{width:"100%", borderRadius:"10px"}}></img>
                     <h4 class="title_2 mx-2" style={{textAlign:"center"}}>{props.title}</h4>
                     {/* <p class="description_2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque.</p> */}
-                    <a href="#" class="read-more"><PiGithubLogoFill ></PiGithubLogoFill></a>
+                    <a href={props.gitlink} class="read-more"><PiGithubLogoFill ></PiGithubLogoFill></a>
                 </div>
             </div>
             </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,11 +6,11 @@ const Projects = () => {
     <div className='my-6' style={{marginTop:"5rem"}}>
     <h1 style={{color:"white",textAlign:"center"}}>Projects</h1>
     <div className='d-flex flex-row flex-wrap justify-content-center m-6 ' style={{marginTop:"3rem",marginLeft:"3.5rem",marginRight:"3.5rem"}}>
-      <ProjCard s={require('../assets/Travel-website.png')} title={"Travel Website"} h={175} w={320} link={"https://travel-website-oaaqcp37w-harshalifarde-gmailcom.vercel.app/"}  ></ProjCard>
-      <ProjCard s={require('../assets/covid-tracker.png')} title={"Covid Tracker"} h={175} w={320} link={'https://covid-tracker-ox28lobd7-harshalifarde-gmailcom.vercel.app/'}></ProjCard>
-      <ProjCard s={require('../assets/digital-clock.png')} title={"Digital Clock"} h={175} w={319} link={"https://digital-clock-b78zaza8y-harshalifarde-gmailcom.vercel.app/"}></ProjCard>
-      <ProjCard s={require('../assets/temp-converter.png')} title={"Temperature Converter"} h={175} w={319} link={'https://temp-converter-57ltsz466-harshalifarde-gmailcom.vercel.app/'}></ProjCard>
-      <ProjCard s={require('../assets/to-do-list.png')} title={"To-Do App"} h={175} w={319} link={"https://to-do-list-nqzuk5pnc-harshalifarde-gmailcom.vercel.app/"}></ProjCard>
+      <ProjCard s={require('../assets/Travel-website.png')} title={"Travel Website"} h={175} w={320} link={"https://travel-website-oaaqcp37w-harshalifarde-gmailcom.vercel.app/"} gitlink={'https://github.com/harshali08/Travel-Website'}  ></ProjCard>
+      <ProjCard s={require('../assets/covid-tracker.png')} title={"Covid Tracker"} h={175} w={320} link={'https://covid-tracker-ox28lobd7-harshalifarde-gmailcom.vercel.app/'} gitlink={'https://github.com/harshali08/covid-tracker'}></ProjCard>
+      <ProjCard s={require('../assets/digital-clock.png')} title={"Digital Clock"} h={175} w={319} link={"https://digital-clock-b78zaza8y-harshalifarde-gmailcom.vercel.app/"} gitlink={'https://github.com/harshali08/digital_clock'}></ProjCard>
+      <ProjCard s={require('../assets/temp-converter.png')} title={"Temperature Converter"} h={175} w={319} link={'https://temp-converter-57ltsz466-harshalifarde-gmailcom.vercel.app/'} gitlink={'https://github.com/harshali08/Temp-Converter'}></ProjCard>
+      <ProjCard s={require('../assets/to-do-list.png')} title={"To-Do App"} h={175} w={319} link={"https://to-do-list-nqzuk5pnc-harshalifarde-gmailcom.vercel.app/"} gitlink={'https://github.com/harshali08/to-do-list'}></ProjCard>
    
     </div>
     </div>
