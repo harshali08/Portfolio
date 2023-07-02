a780fc263c9b495a169bd85b41c23a8645db2b01
harshali <harshalifarde@gmail.com>
2023-07-03 00:24:19 +0530

    home page final done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,12 @@ const Home = () => {
   return (
      <>
       <div className='d-flex flex-row flex-wrap justify-content-center home mb-5 ' style={{width:'100%'}}>
-        <div className='ms-5 mt-1'>
+        <div className='ms-3 mt-1 '>
              <div >
               <h4 className='domain'>Hii I'm</h4>
               <h1 className='name' data-text="Harshali Farde">Harshali Farde</h1>
               <h4 className='domain'>Full Stack Developer</h4>
-              <p>sgyhuijhfdfg sdrftgyhuij esdrtfyuh fghj sdfghj styui sdfghjk qwertyuio zxcvbnmwertyu wertyuidfgh</p>
+              <p className='m-2' style={{width:"480px"}}>I am a passionate developer love to build  beautiful ang engaging websites.I always explore to the new technologies and happy to learn new things.</p>
              </div>
              <div className='d-flex flex-row justify-content-left mt-5'>
               
@@ -24,15 +24,17 @@ const Home = () => {
             <a className="btn btn-outline-light btn-floating m-2 p-2" href="https://github.com/harshali08" role="button" >
               <FaGithub fontSize={25}/>
             </a>
-              <a className="btn btn-outline-light btn-floating m-2 p-2" href="" role="button" >
+              <a className="btn btn-outline-light btn-floating m-2 p-2" href="mailto:harshalifarde@gmail.com" role="button" >
               <MdOutlineMailOutline fontSize={25}/>
             </a>
 
              </div>
             
            <div className='d-flex flex-row my-2'>
-             <button class="button-43 me-3" role="button"><a href='#projects'>Projects</a></button>
-             <button class="button-44" role="button"><a href='#about'>About Me</a></button>
+             {/* <button class="button-43 me-3" role="button"><a href='#projects'>Projects</a></button> */}
+             <button class="home-but me-3" role="button"><a href='#about'>About Me</a></button>
+             <button class="home-but" role="button"><a href='#projects'>Projects</a></button>
+
              </div>
            
     
